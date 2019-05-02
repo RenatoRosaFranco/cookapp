aa91aa474cc9000a7f4eec4af3ea5b8e30080873
Renato Rosa Franco <Renato_ny@live.com>
2019-05-02 19:18:26 -0300

    Carreriwave added

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -29,9 +29,9 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     binding.pry
     if @user.persisted?
       sign_in_and_redirect @user, :event => :authentication
-      set_flash_message(:notice, :success, :kind => "Github") if is_navigational_format?
+      set_flash_message(:notice, :success, :kind => "Instagram") if is_navigational_format?
     else
-      session["devise.google_data"] = request.env["omniauth.auth"]
+      session["devise.instagram_data"] = request.env["omniauth.auth"]
       redirect_to new_user_registration_url
     end
   end
